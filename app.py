d667116356be954090d620f82f16708861a627f4
joanitasantiago <joanitamirandasantiago@gmail.com>
2025-03-30 18:37:09 +0200

    feat: TKT-007 - create POST /foods route + Swagger doc

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+from flasgger import Swagger
 from flask import Flask
+from database import db
+from routes.food_routes import food_bp
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notakeout.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+swagger = Swagger(app)
+app.register_blueprint(food_bp)
 
 @app.route("/")
 def home():
     return "API de Gerenciamento de Refeições - MVP"
-
 if __name__ == "__main__":
+    from models.food import Food
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
---
+++ routes/food_routes.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, request, jsonify
+from models.food import Food
+from database import db
+from schemas.food_schema import food_schema
+
+food_bp = Blueprint("food_bp", __name__)
+
+@food_bp.route("/foods", methods=["POST"])
+def create_food():
+    """
+    Cadastrar um novo alimento
+    ---
+    tags:
+      - Alimentos
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            name:
+              type: string
+            category:
+              type: string
+            nutritional_value:
+              type: string
+            unit:
+              type: string
+            in_stock:
+              type: boolean
+    responses:
+      201:
+        description: Alimento criado com sucesso
+    """
+    data = request.get_json()
+    new_food = Food(
+        name=data.get("name"),
+        category=data.get("category"),
+        nutritional_value=data.get("nutritional_value"),
+        unit=data.get("unit"),
+        in_stock=data.get("in_stock", False)
+    )
+    db.session.add(new_food)
+    db.session.commit()
+
+    return jsonify(food_schema(new_food)), 201
